02417276b130b6595a59e0a52637567b9036e913
周上行Ryer <melaiiryer@gmail.com>
2023-09-16 16:50:29 +0800

    fix: allow impl to be mut

--- src/api.rs
+++ src/api.rs
@@ -18,32 +18,32 @@ pub trait AuthAPI {
 
 pub trait UserAPI {
     /// 查询用户信息（不含隐私信息）
-    fn query_user_info(&self, user_id: Id) -> crate::Result<UserInfo>;
+    fn query_user_info(&mut self, user_id: Id) -> crate::Result<UserInfo>;
 
     /// `需要登陆` 获取个人资料
-    fn try_get_profile(&self, user_id: Id, with_private: bool) -> crate::Result<UserInfo>;
+    fn try_get_profile(&mut self, user_id: Id, with_private: bool) -> crate::Result<UserInfo>;
 }
 
 pub trait AuthorAPI {
     /// 查询作者信息
-    fn query_author_info(&self, author_id: Id) -> crate::Result<AuthorInfo>;
+    fn query_author_info(&mut self, author_id: Id) -> crate::Result<AuthorInfo>;
 
     /// 查询作者的作品列表
-    fn query_work_list(&self, author_id: Id) -> crate::Result<Vec<WorkRef>>;
+    fn query_work_list(&mut self, author_id: Id) -> crate::Result<Vec<WorkRef>>;
 }
 
 pub trait NovelAPI {
     /// 查询小说信息
-    fn query_novel_info(&self, novel_id: Id) -> crate::Result<NovelInfo>;
+    fn query_novel_info(&mut self, novel_id: Id) -> crate::Result<NovelInfo>;
 
     /// 查询分卷信息
-    fn query_volume_info(&self, volume_id: Id) -> crate::Result<VolumeInfo>;
+    fn query_volume_info(&mut self, volume_id: Id) -> crate::Result<VolumeInfo>;
 
     /// 查询章节信息（不含章节正文）
-    fn query_chapter_info(&self, chapter_id: Id) -> crate::Result<ChapterInfo>;
+    fn query_chapter_info(&mut self, chapter_id: Id) -> crate::Result<ChapterInfo>;
 
     /// `可选登录` 获取章节正文
     ///
     /// 当目标章节不存在或章节未订阅时获取失败
-    fn try_get_chapter_content(&self, chapter_id: Id) -> crate::Result<String>;
+    fn try_get_chapter_content(&mut self, chapter_id: Id) -> crate::Result<String>;
 }
