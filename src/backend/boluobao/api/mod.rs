f047c287c56fc8dacaad155d5fd82f57caec9589
周上行Ryer <melaiiryer@gmail.com>
2023-09-16 22:19:33 +0800

    feat(backend/boluobao): impl UserAPI

--- src/backend/boluobao/api/mod.rs
+++ src/backend/boluobao/api/mod.rs
@@ -2,5 +2,7 @@ use super::*;
 
 mod auth;
 mod novel;
+mod user;
 pub use auth::*;
 pub use novel::*;
+pub use user::*;
---
+++ src/backend/boluobao/api/user.rs
@@ -0,0 +1,42 @@
+use super::*;
+
+impl crate::api::UserAPI for BoluobaoHost {
+    fn query_user_info(&mut self, user_id: Id) -> crate::Result<UserInfo> {
+        let resp = self
+            .as_guest()
+            .api_get(&format!("/users/{}", user_id))
+            .query(&[("expand", "introduction,avatar,followNum,fansNum")])
+            .send()?;
+
+        let data: types::User = process_response(resp.status(), &resp.text()?)?.unwrap();
+        let user_info = UserInfo::from(data);
+
+        Ok(UserInfo {
+            private_info: None,
+            ..user_info
+        })
+    }
+
+    fn try_get_profile(&mut self, user_id: Id, with_private: bool) -> crate::Result<UserInfo> {
+        let basic_user_info = self.query_user_info(user_id)?;
+
+        if let Some(host) = self.as_auth(user_id) {
+            let resp = host.api_get(&format!("/user")).send()?;
+
+            let data: types::User = process_response(resp.status(), &resp.text()?)?.unwrap();
+            let user_info = UserInfo::from(data);
+
+            Ok(UserInfo {
+                is_author: user_info.is_author,
+                private_info: if with_private {
+                    user_info.private_info
+                } else {
+                    None
+                },
+                ..basic_user_info
+            })
+        } else {
+            anyhow::bail!("unauthorized user")
+        }
+    }
+}
